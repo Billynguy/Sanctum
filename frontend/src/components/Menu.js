6549801cc0fd0046579858efaeaea0b77f9b1637
Billy <billynguy20@gmail.com>
2024-02-22 23:42:11 -0600

    fixed spacing

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -12,65 +12,69 @@ import DensitySmallIcon from '@mui/icons-material/DensitySmall';
 import { Link } from 'react-router-dom';
 
 export default function TemporaryDrawer() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (newOpen) => () => {
+    const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
-  };
+    };
 
-  const DrawerList = (
-    <Box sx={{ width: 500 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-      <ListItem>
-            <ListItemButton>
-                <Link to="/">
-                    <ListItemText 
-                        primary="Home"
-                        primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                    />
-                </Link>
-            </ListItemButton>
-        </ListItem>
-        <ListItem>
-            <ListItemButton>
-                <Link to="/upload">
-                    <ListItemText 
-                        primary="Upload Data"
-                        primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                    />
-                </Link>
-            </ListItemButton>
-        </ListItem>
-        <ListItem>
-            <ListItemButton>
-                <Link to="/explore">
-                    <ListItemText 
-                        primary="Explore Data"
-                        primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                    />
-                </Link>
-            </ListItemButton>
-        </ListItem>
-        <ListItem>
-            <ListItemButton>
-                <Link to="/login">
-                    <ListItemText 
-                        primary="Log In"
-                        primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                    />
-                </Link>
-            </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
-  );
+    const DrawerList = (
+        <Box sx={{ width: 500 }} role="presentation" onClick={toggleDrawer(false)}>
+            <List>
+            <ListItem>
+                <ListItemButton>
+                    <Link to="/">
+                        <ListItemText 
+                            primary="Home"
+                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
+                        />
+                    </Link>
+                </ListItemButton>
+            </ListItem>
 
-  return (
+            <ListItem>
+                <ListItemButton>
+                    <Link to="/upload">
+                        <ListItemText 
+                            primary="Upload Data"
+                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
+                        />
+                    </Link>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem>
+                <ListItemButton>
+                    <Link to="/explore">
+                        <ListItemText 
+                            primary="Explore Data"
+                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
+                        />
+                    </Link>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem>
+                <ListItemButton>
+                    <Link to="/login">
+                        <ListItemText 
+                            primary="Log In"
+                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
+                        />
+                    </Link>
+                </ListItemButton>
+            </ListItem>
+
+            </List>
+        </Box>
+    );
+
+return (
     <div>
-      <Button onClick={toggleDrawer(true)}><DensitySmallIcon/></Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
+        <Button onClick={toggleDrawer(true)}><DensitySmallIcon/></Button>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+            {DrawerList}
+        </Drawer>
     </div>
-  );
+    );
 }

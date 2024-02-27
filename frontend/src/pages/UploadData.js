239f8cf8627885f5e4eb1c5c6f409f9b396673dc
Billy <billynguy20@gmail.com>
2024-02-26 19:13:37 -0600

    reverted uploaddata to class component to fix functionality

--- frontend/src/pages/UploadData.js
+++ frontend/src/pages/UploadData.js
@@ -1,30 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from '@mui/material/Button';
 import axios from 'axios';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Menu from "../components/Menu";
 import "../styles/uploadData.css";
 
-const UploadData = () => {
-    const [files, setFiles] = useState([]);
-    const [uploadedFiles, setUploadedFiles] = useState([]);
-    const [status, setStatus] = useState("initial");
+const Result = ({ status }) => {
+    if (status === "success") {
+        return <p1>✅ Uploaded successfully!</p1>;
+    } else if (status === "fail") {
+        return <p1>❌ Upload failed!</p1>;
+    } else if (status === "uploading") {
+        return <p1>⏳ Uploading started...</p1>;
+    } else {
+        return null;
+    }
+};
 
-    const handleFileChange = (event) => {
+class UploadData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: [],
+            uploadedFiles: [],
+            status: "initial"
+        };
+    }
+
+    handleFileChange = (event) => {
         event.preventDefault();
-        setFiles([...event.target.files]);
-        setStatus("initial");
-        console.log('changed');
+        this.setState({ files: [...event.target.files] })
+        this.setState({ status: "initial" })
+        console.log('changed')
     };
 
-    const handleFileSubmit = async (event) => {
+    handleFileSubmit = async (event) => {
         event.preventDefault();
         const url = 'http://localhost:5000/upload';
-        setStatus("uploading");
+        this.setState({ status: "uploading" })
         const formData = new FormData();
-        files.forEach((file, index) => {
+        this.state.files.forEach((file, index) => {
             formData.append('files', file);
-        });
+        })
 
         const config = {
             headers: {
@@ -48,69 +65,59 @@ const UploadData = () => {
         axios.post(url, formData, config)
             .then((response) => {
                 console.log(response.data);
-                setStatus("success");
-                setUploadedFiles(files);
+                this.setState({ status: "success" })
+                this.setState({ uploadedFiles: this.files })
             })
             .catch((error) => {
-                console.error("Error uploading this file");
-                setStatus("fail");
+                console.error("Error uploading this file")
+                this.setState({ status: "fail" })
             });
 
     };
-
-    return (
-        <div className="wholeDataPage">
-            <Menu />
-            <div className="title">
-                <h1>Data Upload</h1>
-                <p>Accepted formats: csv, jpg, zip, gzip</p>
-            </div>
-            <div className="fileForm">
-                <form onSubmit={handleFileSubmit}>
-                    <div className="uploadSection">
-                        <input className="chooseButton" type="file" multiple onChange={handleFileChange}></input>
-                        <div className="uploadContainer">
-                            <Button className="uploadButton" type="submit" color="secondary" component="label" variant="contained" startIcon={<CloudUploadIcon />}>
-                                Upload
-                            </Button>
+    
+    render() {
+        return (
+            <div class="wholeDataPage">
+                <Menu />
+                <div class="title">
+                    <h1>Data Upload</h1>
+                    
+                    <p>Accepted formats: csv, jpg, zip, gzip</p>
+                </div>
+                <div class = "fileForm">
+                    <form onSubmit={this.handleFileSubmit}>
+                        <div class="uploadSection">
+                            <input className = "chooseButton" type="file" multiple onChange={this.handleFileChange}></input>
+                            <div class="uploadContainer">
+                                <Button className="uploadButton" type="submit" color = "secondary" component="label" variant="contained" onClick={this.handleFileSubmit} startIcon={<CloudUploadIcon />}>
+                                    Upload
+                                </Button>
+                            </div>
                         </div>
-                    </div>
-                    {files &&
-                        [...files].map((file, index) => (
-                            <section key={file.name}>
-                                File number {index + 1} details:
-                                <ul>
-                                    <li>Name: {file.name}</li>
-                                    <li>Type: {file.type}</li>
-                                    <li>Size: {(file.size / (1024 * 1024)).toFixed(2)} MB</li>
-                                </ul>
-                            </section>
-                        ))}
-                    <div className="progressContainer">
-                        <Result status={status} />
-                    </div>
-                    <p className="bottom">Data upload details: <br />
-                        Upon upload, all data will be parsed and de-identified in compliance with HIPPA, and tokenized. When a model
-                        is created that uses your data for training, you will receive x tokens by uploading your data, you consent to
-                        allowing the data to be used for training models and grant access rights to Sanctum. <br /><br /> By uploading, you
-                        acknowledge that you own the rights to use patient data for research.
-                    </p>
-
-                </form>
+                        {this.state.files &&
+                            [...this.state.files].map((file, index) => (
+                                <section key={file.name}>
+                                    File number {index + 1} details:
+                                    <ul>
+                                        <li>Name: {file.name}</li>
+                                        <li>Type: {file.type}</li>
+                                        <li>Size: {(file.size / (1024 * 1024)).toFixed(2)} MB</li>
+                                    </ul>
+                                </section>
+                            ))}
+                        <div class = "progressContainer">
+                            <Result status={this.state.status} />
+                        </div>
+                        <p class = "bottom">Data upload details: <br/>
+                            Upon upload, all data will be parsed and de-identified in compliance with HIPPA, and tokenized. When a model
+                            is created that uses your data for training, you will receive x tokens by uploading your data, you consent to
+                            allowing the data to be used for training models and grant access rights to Sanctum. <br></br> <br></br> By uploading, you
+                            acknowledge that you own the rights to use patient data for research.
+                        </p>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
-};
-
-const Result = ({ status }) => {
-    if (status === "success") {
-        return <p>✅ Uploaded successfully!</p>;
-    } else if (status === "fail") {
-        return <p>❌ Upload failed!</p>;
-    } else if (status === "uploading") {
-        return <p>⏳ Uploading started...</p>;
-    } else {
-        return null;
+        );
     }
 };
 

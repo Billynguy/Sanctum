ca529bde73c2048bd3102d1d70b136e1c4aee29f
unknown <umarchoudrysonic@gmail.com>
2024-03-31 18:49:38 -0500

    Added feature where u can only click on upload if u are loggedIn

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+
+import React, { useState, useContext } from "react";
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -8,73 +9,96 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import '../styles/menu.css';
 import DensitySmallIcon from '@mui/icons-material/DensitySmall';
-
+import { SessionContext } from "../contexts/SessionContext";
 import { Link } from 'react-router-dom';
+
+const ConditionalLinkListItem = ({ condition, route, pageName }) => {
+    if (condition) {
+        return (
+            <ListItem>
+                <ListItemButton>
+                    <Link to={route}>
+                        <ListItemText
+                            primary={pageName}
+                            primaryTypographyProps={{ style: { fontSize: '24px' } }}
+                        />
+                    </Link>
+                </ListItemButton>
+            </ListItem>
+        );
+    } else {
+        return (
+            <ListItem>
+                <ListItemButton>
+                    <ListItemText
+                        primary={pageName}
+                        primaryTypographyProps={{ style: { fontSize: '24px' } }}
+                    />
+                </ListItemButton>
+            </ListItem>
+        );
+    }
+};
 
 export default function TemporaryDrawer() {
     const [open, setOpen] = React.useState(false);
-
+    const { session } = useContext(SessionContext);
     const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
+        setOpen(newOpen);
     };
 
     const DrawerList = (
         <Box sx={{ width: 500 }} role="presentation" onClick={toggleDrawer(false)}>
             <List>
-            <ListItem>
-                <ListItemButton>
-                    <Link to="/">
-                        <ListItemText 
-                            primary="Home"
-                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                        />
-                    </Link>
-                </ListItemButton>
-            </ListItem>
+                <ListItem>
+                    <ListItemButton>
+                        <Link to="/">
+                            <ListItemText
+                                primary="Home"
+                                primaryTypographyProps={{ style: { fontSize: '24px' } }}
+                            />
+                        </Link>
+                    </ListItemButton>
+                </ListItem>
 
-            <ListItem>
-                <ListItemButton>
-                    <Link to="/upload">
-                        <ListItemText 
-                            primary="Upload Data"
-                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                        />
-                    </Link>
-                </ListItemButton>
-            </ListItem>
+                <ConditionalLinkListItem
+                    condition={session.loggedIn}
+                    route="/upload"
+                    pageName="Upload Data"
+                />
 
-            <ListItem>
-                <ListItemButton>
-                    <Link to="/explore">
-                        <ListItemText 
-                            primary="Explore Data"
-                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                        />
-                    </Link>
-                </ListItemButton>
-            </ListItem>
+                <ListItem>
+                    <ListItemButton>
+                        <Link to="/explore">
+                            <ListItemText
+                                primary="Explore Data"
+                                primaryTypographyProps={{ style: { fontSize: '24px' } }}
+                            />
+                        </Link>
+                    </ListItemButton>
+                </ListItem>
 
-            <ListItem>
-                <ListItemButton>
-                    <Link to="/login">
-                        <ListItemText 
-                            primary="Log In"
-                            primaryTypographyProps={ {style: {fontSize: '24px'}} }
-                        />
-                    </Link>
-                </ListItemButton>
-            </ListItem>
+                <ListItem>
+                    <ListItemButton>
+                        <Link to="/login">
+                            <ListItemText
+                                primary="Log In"
+                                primaryTypographyProps={{ style: { fontSize: '24px' } }}
+                            />
+                        </Link>
+                    </ListItemButton>
+                </ListItem>
 
             </List>
         </Box>
     );
 
-return (
-    <div>
-        <Button onClick={toggleDrawer(true)}><DensitySmallIcon/></Button>
-        <Drawer open={open} onClose={toggleDrawer(false)}>
-            {DrawerList}
-        </Drawer>
-    </div>
+    return (
+        <div>
+            <Button onClick={toggleDrawer(true)}><DensitySmallIcon /></Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
+        </div>
     );
 }

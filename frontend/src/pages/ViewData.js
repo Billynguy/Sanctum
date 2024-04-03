dc1b5d2f070e4157b394ce84ebbfc4bb0326f3fa
unknown <umarchoudrysonic@gmail.com>
2024-04-03 17:34:02 -0500

    view dataset progress

--- frontend/src/pages/ViewData.js
+++ frontend/src/pages/ViewData.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Menu from "../components/Menu";
 import { useParams } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 //main parent component
 function ViewData(props) {
   const {id} = useParams();
+  const [description, setDescription] = useState(null);
+  const url = 'http://localhost:5000/fetchDescription/' + id;
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setDescription(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div>
       <Menu/>
       <div>
-        <Description title={id}></Description>
+        <Description title={id} description={description}></Description>
       </div>
     </div>
   );

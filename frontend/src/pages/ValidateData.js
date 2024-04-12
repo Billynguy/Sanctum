1fe3fdeed7ecf168bf1f1f605a925c71c36eccfe
unknown <umarchoudrysonic@gmail.com>
2024-04-11 20:10:32 -0500

    validator feature working

--- frontend/src/pages/ValidateData.js
+++ frontend/src/pages/ValidateData.js
@@ -6,14 +6,37 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { useNavigate } from 'react-router-dom';
 import Menu from "../components/Menu";
 import User from "../components/User";
+import ValidatorTable from "../components/ValidatorTable";
+import { formatFileSize, datetimeFormat } from '../components/utils';
 
 function ValidateData(){
-    const { session } = useContext(SessionContext)
+    const { session } = useContext(SessionContext);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate()
 
     useEffect(() => {
-        if (session.loggedIn && session.userType=='Validator') {
-            //whatever
+        if (session.loggedIn && session.userType==='Validator') {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch('http://127.0.0.1:5000/display_nonvalidated_files');
+                    const names = await response.json();
+                    const data = names.map(obj => ({
+                        name: obj.Name,
+                        description: obj.Description,
+                        size: formatFileSize(obj.Size),
+                        modified: datetimeFormat.format(new Date(obj.LastModified)),
+                        uploadedBy: obj.UploadedBy
+                    }));
+                    setData(data);
+                    setLoading(false);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    setLoading(false);
+                }
+            };
+    
+            fetchData();
         }
         else{
             navigate('/login')
@@ -23,6 +46,7 @@ function ValidateData(){
     return (
         <div>
             <h1>Validate Data Page</h1>
+            <ValidatorTable data={data} loading={loading}></ValidatorTable>
         </div>
     );
 }
